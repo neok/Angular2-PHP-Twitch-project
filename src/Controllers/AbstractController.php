0878cf32bd2f3a895153221924cd02978ed25657
Yevgen <developer351@gmail.com>
2016-02-04 14:01:18 +0200

    created abstract metods

--- src/Controllers/AbstractController.php
+++ src/Controllers/AbstractController.php
@@ -5,10 +5,47 @@ namespace Twitch\Controllers;
 
  abstract class AbstractController
 {
+    protected $container;
+    protected $twig;
+    protected $response;
+    protected $request;
+     /**
+      * @param Container $container
+      */
     public function __constructor(Container $container)
     {
         $this->container = $container;
     }
+
+     /**
+      * check whether the key exists , and if so set the value of $ this-twig
+      */
+    public function setTwig(Container $twig)
+    {
+        if(array_key_exists('twig',$twig)){
+            $this->twig = $twig['twig'];
+        }
+    }
+
+     /**
+      * @param Container $response
+      */
+     public function setResponse(Container $response)
+     {
+         if(array_key_exists('response',$response)){
+             $this->response = $response['response'];
+         }
+     }
+
+     /**
+      * @param Container $request
+      */
+     public function setRequest(Container $request)
+     {
+         if(array_key_exists('request',$request)){
+             $this->request = $request['request'];
+         }
+     }
 
     
 }
