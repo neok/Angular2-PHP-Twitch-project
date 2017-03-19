1ef6f6adcbd263cf28b2691d4929cfadbdecb5ec
Peter <petrneok@gmail.com>
2017-03-19 15:47:04 +0200

    added nav menu

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MainComponent } from './components/main-component/main.component';
+import { NavbarComponent } from './components/navbar-component/navbar.component';
 import { _404Component } from './components/404-component/_404.component';
 import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule, routing ],
-  declarations: [ MainComponent, AppComponent, _404Component ],
+  declarations: [ MainComponent, AppComponent, _404Component, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ frontend/src/app/components/navbar-component/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: "navbar-component",
+    templateUrl: './navbar.component.html'
+})
+
+export class NavbarComponent
+{
+
+}
